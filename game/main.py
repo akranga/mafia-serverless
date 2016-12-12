39e44a45c1834c8470de6e1e070479cafb72b10e
Antons Kranga <anton@agilestacks.com>
2016-12-12 12:28:12 +0200

    Initial implementation for game

--- game/main.py
+++ game/main.py
@@ -3,11 +3,71 @@ import os, sys
 script_dir = os.path.dirname( os.path.realpath(__file__) )
 sys.path.insert(0, script_dir + os.sep + "lib")
 
-import logging, random, uuid
+import logging, boto3, uuid, json
+
+import game_controller
 
 log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
+#
+# API Gateway feature called Lambda Proxy relies on
+# response in specific format, otherwise it will trhow 
+# a runtime error.
+# 
+# This is why for proper check incoming event if it looks
+# like an event from API Gateway then make response for Lambda-Proxy form
+#
+def response(body, event, code=200):
+  if 'resource' in event and 'httpMethod' in event:
+    return {
+        'statusCode': code,
+        'headers': {},
+        'body': json.dumps(body, separators=(',', ':')) 
+      }
+  return body
 
-def handler(event, context):
-  return {"Message": "Welcome to the Serverless Workshop fully powered by AWS Lambda elastic cloud computing service"}
+#
+# It all starts withe new game
+# This is a handler method that creates a new game
+# and stores it's state in the database.
+# 
+# TODO: Bootstrap a DynamoDB (check env vars for lambda)
+# Then check game_controller (where logic of Mafia Game encoded)
+# and implement a new game
+#
+def new_game_handler(event, context):
+  return response( {"Message": "Welcome to the Serverless Workshop fully powered by AWS Lambda elastic cloud computing service"}, event)
+
+#
+# This AWS lambda handler method returns state of the game.
+# It will also show if the game has been finished or not
+#
+def game_state_handler(event, context):
+  return response( {"Message": "Welcome to the Serverless Workshop fully powered by AWS Lambda elastic cloud computing service"}, event)
+
+#
+# This function method handles a night event when Mafia chooses an innocent victim and iills it
+# Then it iwll record state in the databaseb
+#
+def night_handler(event, context):
+  return response( {"Message": "Welcome to the Serverless Workshop fully powered by AWS Lambda elastic cloud computing service"}, event)
+
+#
+# This function wil tell you about players accusition process
+# where they accusing each other of murders. Please note Mafia
+# knows their identities and never accuses their brother
+# 
+# Result is not stored in the database. Instead uses gets whole
+# chain of accusitions and must choice person to judge
+#
+def day_handler(event, context):
+  return response( {"Message": "Welcome to the Serverless Workshop fully powered by AWS Lambda elastic cloud computing service"}, event)
+
+#
+# In this function User sends event with suspected person name. It 
+# must come in the form of GET variable in the event
+#
+def judgement_handler(event, context):
+  return response( {"Message": "Welcome to the Serverless Workshop fully powered by AWS Lambda elastic cloud computing service"}, event)
+
